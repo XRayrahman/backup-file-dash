6145042e908faa621369f504e05762dbec11e138
Ray Rahman <44991651+XRayrahman@users.noreply.github.com>
2022-10-18 08:30:03 +0700

    Create ArduCOMM-181022.ino

---
+++ ArduCOMM-181022.ino
@@ -0,0 +1,112 @@
+#include <SoftwareSerial.h>
+#include <OneWire.h>
+#include <DallasTemperature.h>
+//#define SERIAL_TX_BUFFER_SIZE 128
+//#define SERIAL_RX_BUFFER_SIZE 128
+#include <FreqMeasure.h>
+#define ONE_WIRE_BUS 7
+
+SoftwareSerial HM10(2,3); //TX dan RX
+OneWire oneWire(ONE_WIRE_BUS);	
+DallasTemperature sensors(&oneWire);
+
+String inDataSebelum = "";
+String sendData = "";
+String Data = "";
+double sum=0;
+int count=0;
+int rps=0;
+float suhu=0;
+float tegangan=0;
+
+void setup() {
+  Serial.begin(115200);
+  // pinMode(A0, INPUT_PULLUP);
+  HM10.begin(115200);
+  FreqMeasure.begin();
+
+}
+
+void loop() {
+  HM10.listen();
+  
+  //Serial Common Data
+  serial_com();
+
+  //bluetooth
+  bluetooth();
+}
+
+void serial_com()
+  {    
+//    tegangan
+  tegangan = tegangan84v();
+    
+// kecepatan
+  if (FreqMeasure.available()) {
+    // average several reading together
+    sum = sum + FreqMeasure.read();
+    count = count + 1;
+    if (count > 1) {
+      float frequency = FreqMeasure.countToFrequency(sum / count);
+      // need to calibrate more
+      rps = (frequency*120/8.1)/60;
+      if (rps < 0){
+        rps = 0;
+        }
+      sum = 0;
+      count = 0;
+    }
+  }
+  else
+    {rps = 0;}
+    
+//   suhu 
+    suhu = ds18b20();
+    
+    Serial.print("{ \"t\":\"");
+    Serial.print(voltage);
+    Serial.print("\", ");
+    Serial.print("\"s\":\"");
+    Serial.print(suhu);
+    Serial.print("\", ");
+    Serial.print("\"r\":\"");
+    Serial.print(rps);
+    Serial.print("\"");
+  }
+
+void ds18b20(){
+    sensors.setResolution(9);
+    sensors.requestTemperatures();
+    return sensors.getTempCByIndex(0);
+}
+
+void tegangan84v(){
+    float vin = (analogRead(A0));
+    float voltage = vin * (5.0 / 1023.0);
+    return voltage *16.8; 
+  }
+  
+void bluetooth()
+  {
+    while (HM10.available()){
+      // sendData = HM10.readString(); //ada delay read kalau pakai function ini
+      // baca setiap char trus dijadikan satu string
+      char appData = HM10.read();
+      sendData = inDataSebelum + appData;
+      inDataSebelum = sendData;
+      }
+      
+    inDataSebelum = "";
+    Data = sendData;
+    if(sendData=="" || sendData== "stop" || sendData== " ") {
+      Serial.print(", \"isRun\":false }");
+      Serial.println();
+    }
+    else {
+      Serial.print(", ");
+      Serial.print(String(Data));
+      Serial.print(", \"isRun\":true }");
+      Serial.println();
+    }
+  }
